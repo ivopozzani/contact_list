3bf4c1bfb434fb7a55b46b64df364f1ef9eb5772
ivopozzani <ivo.pozzani@gmail.com>
2022-02-11 14:43:55 -0300

    Feat: update main column before validation

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -7,4 +7,17 @@ class Phone < ApplicationRecord
   validates :main, uniqueness: { scope: :contact_id, conditions: lambda {
                                                                    where(main: true)
                                                                  } }, inclusion: { in: [true, false] }
+
+  before_validation :update_main_column
+
+  private
+
+  def update_main_column
+    if main == true
+      phone_number = Phone.find_by(contact_id:, main: true)
+      return if phone_number.nil?
+
+      phone_number.update_column :main, false
+    end
+  end
 end
