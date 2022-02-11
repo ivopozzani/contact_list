558e646f862c66c20dae12d5d7452c859a82befb
ivopozzani <ivo.pozzani@gmail.com>
2022-02-11 14:43:11 -0300

    Feat: has_many through corrected  general refactor

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,5 @@
 class Contact < ApplicationRecord
   belongs_to :user
   has_many :phones
-  has_many :phones, through :contacts
   validates :name, presence: true, uniqueness: { scope: :user_id }
 end
