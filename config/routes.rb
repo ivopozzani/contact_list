5df28b807cb70b4c916c58500290f5a84b4da8dd
ivopozzani <ivo.pozzani@gmail.com>
2022-02-14 16:52:30 -0300

    feat: show contacts added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'users' => 'users#index'
-  get 'users/show'
+  get 'users/:id' => 'users#show'
   get 'users/new'
   get 'users/create'
   get 'users/edit'
