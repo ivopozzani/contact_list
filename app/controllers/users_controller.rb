d25fe169eb1a2d9424f8ed90ba252a41b8fddb9f
ivopozzani <ivo.pozzani@gmail.com>
2022-02-14 14:21:53 -0300

    feat:test implemented /index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-  @users = User.all
+    @users = User.all
   end
 
   def show; end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,55 +5,54 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      pending 'not implemented yet'
-      get '/users/index'
+      get '/users'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/show'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /new' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/new'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /create' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/create'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /edit' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/edit'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /update' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/update'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /destroy' do
+    pending 'not implemented yet'
     it 'returns http success' do
-      pending 'not implemented yet'
       get '/users/destroy'
       expect(response).to have_http_status(:success)
     end
