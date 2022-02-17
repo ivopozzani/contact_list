880807c5fc9634c9fb2ca282d5df86ac0a0ae1ab
ivopozzani <84991192+ivopozzani@users.noreply.github.com>
2022-02-17 09:15:04 -0300

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
       user = create(:user)
-      get users_id_path(user)
+      get user_path(user)
       expect(response).to have_http_status(:success)
     end
 
