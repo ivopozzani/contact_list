001511151c7d18f0ee7c0893ab18de5322c7fb7d
ivopozzani <ivo.pozzani@gmail.com>
2022-02-17 13:48:20 -0300

    feat: new tests added

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -38,14 +38,28 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'POST /users' do
-    subject { post users_path, params: {user: { name:'lorem', age:32, biography:'lorem lorem lorem' }} }
-    
-    it 'returns http found' do
-      expect(subject).to eq(302)
+    context 'when valid' do
+      subject { post users_path, params: { user: { name: 'lorem', age: 32, biography: 'lorem lorem lorem' } } }
+
+      it 'returns http found' do
+        expect(subject).to eq(302)
+      end
+
+      it 'redirects_to /users/:id' do
+        expect(subject).to redirect_to("/users/#{assigns(:user).id}")
+      end
+
+      it 'add user to db' do
+        subject
+        expect(User.all.count).to eq(1)
+      end
     end
 
-    it 'redirects_to /users/:id' do
-      expect(subject).to redirect_to("/users/#{assigns(:user).id}")
+    context 'when not valid' do
+      it 'does not add user to db' do
+        post users_path, params: { user: { name: '' } }
+        expect(assigns(:user).errors.full_messages_for(:name)).to eq(["Name can't be blank"])
+      end
     end
   end
 
