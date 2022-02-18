082560770e2180ab87de04a55236a271d0b580c1
ivopozzani <84991192+ivopozzani@users.noreply.github.com>
2022-02-18 14:22:40 -0300

    Tarefa7_4 (edit and update User) (#10)
    * feat: show contacts added
    
    * feat: base style updated
    
    * feat: tests updated
    
    * refactor: update tests
    
    * refactor
    
    * feat: add age and biography
    
    * feat: tests added
    
    * refactor: phone query simplified
    
    * refactor: tests updated
    
    * refactor: refactor after review
    
    * refactor
    
    * feat: new form to create user
    
    * feat: add link navigation
    
    * feat: added tests to create user
    
    * feat:added tests to create user
    
    * feat:update style
    
    * Update users_spec.rb
    
    * Update new.html.erb
    
    * update routes.rb
    
    * feat: new tests added
    
    * refactor: refact table and css
    
    * feat: added update form
    
    * feat: add tests and routes
    
    * refactor: update route name
    
    * feat: tests and navigation links added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,19 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
   def destroy; end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/new' => 'users#new'
   post 'users' => 'users#create'
-  get 'users/edit'
-  get 'users/update'
+  get 'users/:id/edit' => 'users#edit', as: 'users_id_edit'
+  patch 'users/:id' => 'users#update'
   get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/:id' => 'users#show', as: 'user'
