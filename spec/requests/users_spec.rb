0c5187cc5502f3c024c2414c6a2a2fdd4e0a354f
ivopozzani <84991192+ivopozzani@users.noreply.github.com>
2022-02-18 14:18:39 -0300

    Tarefa7_3 - For User (new and create) (#9)
    * feat: show contacts added
    
    * feat: base style updated
    
    * feat: tests updated
    
    * refactor: update tests
    
    * refactor
    
    * feat: add age and biography
    
    * feat: tests added
    
    * refactor: phone query simplified
    
    * refactor: tests updated
    
    * refactor: refactor after review
    
    * refactor
    
    * feat: new form to create user
    
    * feat: add link navigation
    
    * feat: added tests to create user
    
    * feat:added tests to create user
    
    * feat:update style
    
    * Update users_spec.rb
    
    * Update new.html.erb
    
    * update routes.rb
    
    * feat: new tests added
    
    * refactor: refact table and css

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -31,18 +31,35 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET /new' do
-    pending 'not implemented yet'
     it 'returns http success' do
-      get '/users/new'
+      get users_new_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /create' do
-    pending 'not implemented yet'
-    it 'returns http success' do
-      get '/users/create'
-      expect(response).to have_http_status(:success)
+  describe 'POST /users' do
+    context 'when valid' do
+      subject { post users_path, params: { user: { name: 'lorem', age: 32, biography: 'lorem lorem lorem' } } }
+
+      it 'returns http found' do
+        expect(subject).to eq(302)
+      end
+
+      it 'redirects_to /users/:id' do
+        expect(subject).to redirect_to("/users/#{assigns(:user).id}")
+      end
+
+      it 'add user to db' do
+        subject
+        expect(User.all.count).to eq(1)
+      end
+    end
+
+    context 'when not valid' do
+      it 'does not add user to db' do
+        post users_path, params: { user: { name: '' } }
+        expect(assigns(:user).errors.full_messages_for(:name)).to eq(["Name can't be blank"])
+      end
     end
   end
 
